90c5a4ded635427d2f59b1542c7534d7af8cb3e2
jiji3000 <tokyo.orinpic@gmail.com>
2018-02-07 18:20:53 +0900

    add peripheralActivity and centralActivity

---
+++ app/src/main/java/jiji4000/jp/androidbluetoothsample/CentralActivity.kt
@@ -0,0 +1,12 @@
+package jiji4000.jp.androidbluetoothsample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CentralActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_central)
+    }
+}
--- app/src/main/java/jiji4000/jp/androidbluetoothsample/MainActivity.kt
+++ app/src/main/java/jiji4000/jp/androidbluetoothsample/MainActivity.kt
@@ -1,12 +1,26 @@
 package jiji4000.jp.androidbluetoothsample
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var centralButton : Button
+    lateinit var peripheralButton : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // instantiate
+        centralButton = findViewById(R.id.central_btn)
+        centralButton.setOnClickListener {
+            startActivity(Intent(this,CentralActivity::class.java))
+        }
+        peripheralButton = findViewById(R.id.periphera_btn)
+        peripheralButton.setOnClickListener {
+            startActivity(Intent(this,PeripheralActivity::class.java))
+        }
     }
 }
