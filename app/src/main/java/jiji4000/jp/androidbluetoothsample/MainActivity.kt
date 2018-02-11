eb996d639c409b520d38b2406e2eb731cc5f1c92
jiji4000 <jiji4000alpha@gmail.com>
2018-02-11 13:30:22 +0900

    add permission dialog

--- app/src/main/java/jiji4000/jp/androidbluetoothsample/MainActivity.kt
+++ app/src/main/java/jiji4000/jp/androidbluetoothsample/MainActivity.kt
@@ -1,26 +1,143 @@
 package jiji4000.jp.androidbluetoothsample
 
+import android.Manifest
+import android.app.AlertDialog
+import android.app.Dialog
+import android.app.DialogFragment
+import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.util.Log
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
-    lateinit var centralButton : Button
-    lateinit var peripheralButton : Button
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+        private const val PERMISSION_DIALOG = "permission_dialog"
+        private const val REQUEST_BLUETOOTH_PERMISSIONS = 1
+        private val BLE_PERMISSIONS = arrayOf(Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN)
+    }
+
+    lateinit var centralButton: Button
+    lateinit var peripheralButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // check permissions for 6.0
+        if (!hasPermissionsGranted(BLE_PERMISSIONS)) {
+            requestBlePermissions()
+            return
+        }
+
         // instantiate
         centralButton = findViewById(R.id.central_btn)
         centralButton.setOnClickListener {
-            startActivity(Intent(this,CentralActivity::class.java))
+            startActivity(Intent(this, CentralActivity::class.java))
         }
         peripheralButton = findViewById(R.id.periphera_btn)
         peripheralButton.setOnClickListener {
-            startActivity(Intent(this,PeripheralActivity::class.java))
+            startActivity(Intent(this, PeripheralActivity::class.java))
+        }
+    }
+
+    /**
+     * Gets whether you should show UI with rationale for requesting permissions.
+     *
+     * @param permissions The permissions your app wants to request.
+     * @return Whether you can show permission rationale UI.
+     */
+    private fun shouldShowRequestPermissionRationale(permissions: Array<String>): Boolean {
+        for (permission in permissions) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
+                return true
+            }
+        }
+        return false
+    }
+
+    /**
+     * Requests permissions needed for recording video.
+     */
+    private fun requestBlePermissions() {
+        if (shouldShowRequestPermissionRationale(BLE_PERMISSIONS)) {
+            ConfirmationDialog().show(fragmentManager, PERMISSION_DIALOG)
+        } else {
+            ActivityCompat.requestPermissions(this, BLE_PERMISSIONS, REQUEST_BLUETOOTH_PERMISSIONS)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
+                                            grantResults: IntArray) {
+        Log.d(TAG, "onRequestPermissionsResult")
+        if (requestCode == REQUEST_BLUETOOTH_PERMISSIONS) {
+            if (grantResults.size == BLE_PERMISSIONS.size) {
+                for (result in grantResults) {
+                    if (result != PackageManager.PERMISSION_GRANTED) {
+                        ErrorDialog.newInstance(getString(R.string.permission_request))
+                                .show(fragmentManager, PERMISSION_DIALOG)
+                        break
+                    }
+                }
+            } else {
+                ErrorDialog.newInstance(getString(R.string.permission_request))
+                        .show(fragmentManager, PERMISSION_DIALOG)
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+
+    /**
+     * check permission granted
+     */
+    private fun hasPermissionsGranted(permissions: Array<String>): Boolean {
+        for (permission in permissions) {
+            if (ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false
+            }
+        }
+        return true
+    }
+
+    class ConfirmationDialog : DialogFragment() {
+        override fun onCreateDialog(savedInstanceState: Bundle): Dialog {
+            val parent = parentFragment
+            return AlertDialog.Builder(activity)
+                    .setMessage("need Bluetooth permission")
+                    .setPositiveButton(android.R.string.ok, DialogInterface.OnClickListener { dialog, which ->
+                        ActivityCompat.requestPermissions(activity, BLE_PERMISSIONS,
+                                REQUEST_BLUETOOTH_PERMISSIONS)
+                    })
+                    .setNegativeButton(android.R.string.cancel,
+                            DialogInterface.OnClickListener { dialog, which -> parent.activity.finish() })
+                    .create()
+        }
+    }
+
+    class ErrorDialog : DialogFragment() {
+        override fun onCreateDialog(savedInstanceState: Bundle): Dialog {
+            val activity = activity
+            return AlertDialog.Builder(activity)
+                    .setMessage(arguments.getString(ARG_MESSAGE))
+                    .setPositiveButton(android.R.string.ok) { dialogInterface, i -> activity.finish() }
+                    .create()
+        }
+
+        companion object {
+            private val ARG_MESSAGE = "message"
+            fun newInstance(message: String): ErrorDialog {
+                val dialog = ErrorDialog()
+                val args = Bundle()
+                args.putString(ARG_MESSAGE, message)
+                dialog.arguments = args
+                return dialog
+            }
         }
     }
 }
